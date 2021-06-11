4ef88c357ce10bd941092a3a19edf2f698259578
Lucas Luz de Sena Neves <lucas.neves@ints.local>
2021-06-11 11:07:49 -0300

    add reset css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
-
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>hello</h1>
+       <GlobalStyle/>
       </header>
     </div>
   );
