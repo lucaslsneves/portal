387af5636434d07f5a7645b36952b7b32ec8690b
Lucas Luz <lucasluzthatsme@gmail.com>
2021-08-02 08:59:59 -0300

    remove Projetos TIC - INTS (v2)

--- src/App.js
+++ src/App.js
@@ -31,13 +31,6 @@ function App() {
 
         <h1>TIC</h1>
         <CardSection>
-          <Card
-            url="https://app.powerbi.com/groups/me/reports/26311468-e27e-4724-8e50-c6d9271eb6fd/ReportSection25dafee6c7070657804b"
-            title="Projetos TIC - INTS (v2)"
-            description="Dashboard para monitoramento dos projetos do setor de TI no Jira"
-            color="#639cd9"
-        
-          />
          
           <Card
             url="https://app.powerbi.com/groups/me/reports/36a677ff-ecbe-4d7d-a952-82092cfc50fb/ReportSection"
@@ -61,7 +54,7 @@ function App() {
             title="Governança de Contratos - INTS"
             description="Dashboard de Governça de contratos"
             icon="file"
-          
+
           />
       </CardSection>
        
